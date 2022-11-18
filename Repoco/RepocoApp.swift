ea5c4c551d9e90614a50129be754cf048cff888f
alexanderhuebner <alexhuebner@me.com>
2022-11-18 21:45:38 +0100

    Initial Commit

---
+++ Repoco/RepocoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepocoApp.swift
+//  Repoco
+//
+//  Created by Alexander Huebner on 18.11.22.
+//
+
+import SwiftUI
+
+@main
+struct RepocoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
